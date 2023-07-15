56489996b5d349832d92eb58b35275b27664a6bd
Rshabh <126173527+Rishabh-butterpaper@users.noreply.github.com>
2023-07-15 14:27:11 +0530

    some changes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,7 +6,7 @@ export default function ProfilePage() {
       <Navbar/>
       <div className="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
         {/* <img className="w-32 h-32 rounded-full mx-auto" src="https://picsum.photos/200" alt="Profile picture"> */}
-        <h2 className="text-center text-2xl font-semibold mt-3">John Doe</h2>
+        <h2 className="text-center text-2xl font-semibold mt-3">John cena</h2>
         <p className="text-center text-gray-600 mt-1">Software Engineer</p>
         <div className="flex justify-center mt-5">
           <a href="#" className="text-blue-500 hover:text-blue-700 mx-3">
