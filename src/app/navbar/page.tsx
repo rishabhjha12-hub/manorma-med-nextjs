797fdeb42797f97522ad9e25a8b09d83913483d1
Rshabh <126173527+Rishabh-butterpaper@users.noreply.github.com>
2023-07-15 18:33:11 +0530

    added full login logout and profile

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 export default function Navbar() {
+  const router = useRouter();
+  const [user, setUser] = useState("");
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+ useEffect(() => {
+   console.log("from nav");
+     getUserDetails();
+ }, []); 
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setUser(res.data.data._id);
+    if (res.data.data._id) {
+      router.push(`/profile/${res.data.data._id}`);
+    }
+  };
   return (
+    
     <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-indigo-500 mb-3">
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
@@ -18,6 +47,7 @@ export default function Navbar() {
             <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
           </button>
         </div>
+
         <div
           className="lg:flex flex-grow items-center"
           id="example-navbar-warning"
@@ -31,30 +61,47 @@ export default function Navbar() {
                 Home
               </a>
             </li>
-            <li className="nav-item">
-              <a
-                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                href="/login"
-              >
-                Login
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                href="/signup"
-              >
-                Signup
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                href="/profile"
-              >
-                Profile
-              </a>
-            </li>
+
+            {!user && (
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="/login"
+                >
+                  Login
+                </a>
+              </li>
+            )}
+            {!user && (
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="/signup"
+                >
+                  Signup
+                </a>
+              </li>
+            )}
+            {user && (
+              <li className="nav-item">
+                <p
+                  onClick={getUserDetails}
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                >
+                  profile
+                </p>
+              </li>
+            )}
+            {user && (
+              <li className="nav-item">
+                <p
+                  onClick={logout}
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                >
+                  Logout
+                </p>
+              </li>
+            )}
           </ul>
         </div>
       </div>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,32 +1,28 @@
+"use client"
 import Navbar from "@/app/navbar/page";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function ProfilePage() {
+   const [user, setUser] = useState({});
+   useEffect(() => {
+     getUserDetails();
+   }, []);
+   const getUserDetails = async () => {
+     const res = await axios.get("/api/users/me");
+     console.log(res.data);
+     setUser(res.data.data);
+   };
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div className="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
         {/* <img className="w-32 h-32 rounded-full mx-auto" src="https://picsum.photos/200" alt="Profile picture"> */}
-        <h2 className="text-center text-2xl font-semibold mt-3">John cena</h2>
-        <p className="text-center text-gray-600 mt-1">Software Engineer</p>
-        <div className="flex justify-center mt-5">
-          <a href="#" className="text-blue-500 hover:text-blue-700 mx-3">
-            Twitter
-          </a>
-          <a href="#" className="text-blue-500 hover:text-blue-700 mx-3">
-            LinkedIn
-          </a>
-          <a href="#" className="text-blue-500 hover:text-blue-700 mx-3">
-            GitHub
-          </a>
-        </div>
-        <div className="mt-5">
-          <h3 className="text-xl font-semibold">Bio</h3>
-          <p className="text-gray-600 mt-2">
-            John is a software engineer with over 10 years of experience in
-            developing web and mobile applications. He is skilled in JavaScript,
-            React, and Node.js.
-          </p>
-        </div>
+        <h2 className="text-center text-2xl font-semibold mt-3">
+          {user?.username}
+        </h2>
+        <p className="text-center text-gray-600 mt-1">{user?.email}</p>
+        
       </div>
     </div>
   );
