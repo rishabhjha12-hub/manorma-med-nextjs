9d2af81c0ada1f2eb1298f87043f04c2d9a9f850
Anshuman Negi <negsanshuman14@gmail.com>
2023-07-19 17:28:24 +0530

    add button in HeroComponent

--- src/app/HeroComponent/page.tsx
+++ src/app/HeroComponent/page.tsx
@@ -1,29 +1,23 @@
+"use client"
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import HeroComponentImage from "../../assets/hero-product-img.png";
+import {useState} from "react";
 
 export default function HeroComponent() {
-  return (
-    // <div className="max-w-[120rem] my-0 mx-auto">
-    //    <div className="grid gap-9 grid-cols-2">
-    //       <div className="hero-section-data">
-    //         <p className="intro-data my-8 mx-0 mb-0">Welcome to </p>
-    //         <h1 className="capitalize font-bold">Manorma Store</h1>
-    //         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est iusto minima laboriosam beatae. Id sapiente repudiandae facilis quis, aliquid dolorum!</p>
-    //       </div>
-    //       <div>
-    //       {/* Hero-Section-Image */}
-    //          <div className="hero-section-image w-full h-auto flex justify-center items-center">
-    //             <figure className="relative after:content-[''] after:w-1/2 after:h-full after:left-0 after:top-[10%] bg-[#5138ee66]">
-    //                 <Image src={HeroComponentImage} alt="hero-section-image" className="min-w-[10rem] h-40 " />
-    //             </figure>
-    //          </div>
-    //       </div>
-    //    </div>
-    // </div>
 
-    <div className="flex-none h-[50vh] w-full lg:flex lg:h-1/4 lg:justify-center lg:truncate">
+    const [loader, setLoader] = useState(false);
+
+    const clickBtn = () => {
+        
+        console.log("Clicked")
+    }
+
+  return (
+
+    <div className="flex-none h-[60vh] w-full lg:flex lg:h-1/4 lg:justify-center lg:truncate">
         <div className="hero-section-data flex-none w-full h-1/3 p-2 lg:flex lg:flex-col lg:justify-center lg:items-center lg:w-[35%] lg:whitespace-break-spaces">
           <h1 className="capitalize font-bold mb-[1rem] text-2xl lg:text-3xl">Welcome to Manorma Store</h1>
           <p>
@@ -31,6 +25,15 @@ export default function HeroComponent() {
             minima laboriosam beatae. Id sapiente repudiandae facilis quis,
             aliquid dolorum!
           </p>
+          <button
+                type="button"
+                onClick={clickBtn}
+                className="inline-flex items-center px-4 py-2 ml-4 mt-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-900 border border-transparent rounded-md active:bg-gray-900 false"
+                // disabled={loader ? true : false}
+              >
+                {loader?"Clicked.....":"Click Me"}
+                
+              </button>
         </div>
         {/* Hero-Section-Image */}
         <div className="hero-section-image flex-none w-full h-2/3 p-2 lg:flex lg:w-[35%] lg:justify-center lg:items-center ">
