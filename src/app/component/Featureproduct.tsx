9279cfb426061e412cc45bcdb8661c2675d93d5b
Anshuman Negi <negsanshuman14@gmail.com>
2023-07-31 19:23:46 +0530

    Improve Footer

--- src/app/component/Featureproduct.tsx
+++ src/app/component/Featureproduct.tsx
@@ -1,43 +1,43 @@
 "use client";
 
-
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "../component/Card";
-import Link from 'next/link'
 import Shimmer from "./Shimmer";
 
-export default function  FeaturedProduct() {
+export default function FeaturedProduct() {
+  const [labTests, setLabTests] = useState([]);
+  const [filteredLabTests, setFilteredLabTests] = useState([]);
 
-    const [labTests, setLabTests] = useState([]);
-    const [filteredLabTests, setFilteredLabTests] = useState([]);
+  useEffect(() => {
+    async function fetchLabTests() {
+      try {
+        const response = await axios.get("/api/getAllLabtest");
+        setLabTests(response.data);
+        setFilteredLabTests(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching lab tests:");
+      }
+    }
+    fetchLabTests();
+  }, [filteredLabTests]);
 
-    useEffect(() => {
-        async function fetchLabTests() {
-          try {
-            const response = await axios.get("/api/getAllLabtest");
-            setLabTests(response.data);
-            setFilteredLabTests(response.data);
-            console.log(response.data);
-          } catch (error) {
-            console.error("Error fetching lab tests:");
-          }
-        }
-        fetchLabTests();
-      }, [filteredLabTests]);
-  
+  if (filteredLabTests.length === 0) return <Shimmer />;
 
-      if(filteredLabTests.length === 0) return <Shimmer/>
-
-    return (
-          <div className="w-full flex flex-wrap items-center justify-evenly py-3 px-8">           
-
-              {filteredLabTests.map((test:any)=> (
-               test.isFeatured == true ? (
+  return (
+    <div className="flex flex-col">
+     <div className="flex justify-center items-center my-6 ">
+          <h1 className="text-4xl border-b-2 border-sky-500 font-serif">Featured Tests</h1>
+       </div>
+      <div className="w-full flex flex-wrap items-center justify-evenly py-3 px-8">
+        {filteredLabTests.map((test: any) =>
+          test.isFeatured == true ? (
             //   <Card key={test._id} resData={test}  />
-           <Card key={test._id} resData={test}  labData ={labTests} />
-               ) : (null)
-            ))} 
-          </div>
-    );
+            <Card key={test._id} resData={test} labData={labTests} />
+          ) : null
+        )}
+      </div>
+    </div>
+  );
 }
--- src/app/component/Footer.tsx
+++ src/app/component/Footer.tsx
@@ -1,81 +1,252 @@
-import React from "react";
+// import React from "react";
+
+// const Footer = () => {
+//   return (
+//     <footer className="bg-neutral-200   text-center text-white dark:bg-neutral-600 bottom-0 left-0 w-full">
+//       <div className=" pt-4">
+//         <div className="mb-4 flex justify-around">
+//           <a href="#!" className=" text-neutral-800 dark:text-neutral-200">
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               className="h-6 w-6"
+//               fill="currentColor"
+//               viewBox="0 0 24 24"
+//             >
+//               <path
+//                 fillRule="evenodd"
+//                 d="M14.032 4H9.96c-2.967 0-4.96 1.933-4.96 4.933v4.134H2v3.934h3.008V20h4.012v-7h2.995l.736-3.934h-3.73V8.113c0-.904.353-1.774 1.181-1.774h2.91v-3.34c-.508-.07-1.36-.175-2.582-.175-2.53 0-4.22 1.462-4.22 4.08v2.307H3.996v-4C3.996 4.35 5.606 2 9.793 2c1.478 0 2.808.105 3.185.155z"
+//                 clipRule="evenodd"
+//               />
+//             </svg>
+//           </a>{" "}
+//           <a href="#!" className=" text-neutral-800 dark:text-neutral-200">
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               className="h-6 w-6"
+//               fill="currentColor"
+//               viewBox="0 0 24 24"
+//             >
+//               <path
+//                 fillRule="evenodd"
+//                 d="M14.032 4H9.96c-2.967 0-4.96 1.933-4.96 4.933v4.134H2v3.934h3.008V20h4.012v-7h2.995l.736-3.934h-3.73V8.113c0-.904.353-1.774 1.181-1.774h2.91v-3.34c-.508-.07-1.36-.175-2.582-.175-2.53 0-4.22 1.462-4.22 4.08v2.307H3.996v-4C3.996 4.35 5.606 2 9.793 2c1.478 0 2.808.105 3.185.155z"
+//                 clipRule="evenodd"
+//               />
+//             </svg>
+//           </a>{" "}
+//           <a href="#!" className=" text-neutral-800 dark:text-neutral-200">
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               className="h-6 w-6"
+//               fill="currentColor"
+//               viewBox="0 0 24 24"
+//             >
+//               <path
+//                 fillRule="evenodd"
+//                 d="M14.032 4H9.96c-2.967 0-4.96 1.933-4.96 4.933v4.134H2v3.934h3.008V20h4.012v-7h2.995l.736-3.934h-3.73V8.113c0-.904.353-1.774 1.181-1.774h2.91v-3.34c-.508-.07-1.36-.175-2.582-.175-2.53 0-4.22 1.462-4.22 4.08v2.307H3.996v-4C3.996 4.35 5.606 2 9.793 2c1.478 0 2.808.105 3.185.155z"
+//                 clipRule="evenodd"
+//               />
+//             </svg>
+//           </a>{" "}
+//           <a href="#!" className=" text-neutral-800 dark:text-neutral-200">
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               className="h-6 w-6"
+//               fill="currentColor"
+//               viewBox="0 0 24 24"
+//             >
+//               <path
+//                 fillRule="evenodd"
+//                 d="M14.032 4H9.96c-2.967 0-4.96 1.933-4.96 4.933v4.134H2v3.934h3.008V20h4.012v-7h2.995l.736-3.934h-3.73V8.113c0-.904.353-1.774 1.181-1.774h2.91v-3.34c-.508-.07-1.36-.175-2.582-.175-2.53 0-4.22 1.462-4.22 4.08v2.307H3.996v-4C3.996 4.35 5.606 2 9.793 2c1.478 0 2.808.105 3.185.155z"
+//                 clipRule="evenodd"
+//               />
+//             </svg>
+//           </a>
+//           {/* Add more social links here */}
+//         </div>
+//       </div>
+
+//       <div className="bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
+//         © 2023 Copyright:
+//         <a
+//           className="text-neutral-800 dark:text-neutral-400"
+//           href="https://tailwind-elements.com/"
+//         >
+//           Oxign
+//         </a>
+//       </div>
+//     </footer>
+
+
+//   );
+// };
+
+// export default Footer;
+
+
+
+// import React from 'react'
+// import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from "react-icons/ai";
+
+// const Footer = () => {
+//     return (
+//         <>
+     
+
+            // <div className="bg-[#090c31] h-32 w-full flex justify-end items-center">
+            //     <div className='bg-[#5853ff] w-[30%] h-[70%] p-2 flex flex-col items-center rounded-tl-[2rem] rounded-tr-none rounded-br-none rounded-bl-[2rem]'>
+                
+            //         <article className='flex mx-1 my-0 flex-wrap justify-center'>
+            //             <div className='m-2 h-14 w-14 bg-white rounded-[50%] flex flex-col items-center justify-center text-black cursor-pointer transition-all duration-500 hover:invert animate-waving-hand' style={{
+            //                 animationDelay: "0.3s",
+            //                 }}>
+            //                 <AiFillGoogleCircle className='text-2xl'/>
+            //                 <p className='text-[8px] font-black color-[#090c31]'>Google</p>
+            //             </div>
+            //             <div className='m-2 h-14 w-14 bg-white rounded-[50%] flex flex-col items-center justify-center text-black cursor-pointer transition-all duration-500 hover:invert animate-waving-hand' style={{
+            //                 animationDelay: "0.5s",
+            //                 }}>
+            //                 <AiFillAmazonCircle className='text-2xl'/>
+            //                 <p className='text-[8px] font-black color-[#090c31]'>Google</p>
+            //             </div>
+            //             <div className='m-2 h-14 w-14 bg-white rounded-[50%] flex flex-col items-center justify-center text-black cursor-pointer transition-all duration-500 hover:invert animate-waving-hand' style={{
+            //                 animationDelay: "0.7s",
+            //                 }}>
+            //                 <AiFillYoutube className='text-2xl'/>
+            //                 <p className='text-[8px] font-black color-[#090c31]'>Google</p>
+            //             </div>
+            //             <div className='m-2 h-14 w-14 bg-white rounded-[50%] flex flex-col items-center justify-center text-black cursor-pointer transition-all duration-500 hover:invert animate-waving-hand' style={{
+            //                 animationDelay: "1s",
+            //                 }}>
+            //                 <AiFillInstagram className='text-2xl'/>
+            //                 <p className='text-[8px] font-black color-[#090c31]'>Google</p>
+            //             </div>
+
+                 
+            //         </article>
+            //     </div>
+            // </div>
+//         </>
+//     );
+// };
+
+// export default Footer;
+
+
+
+
+
+
+
+
+import { Box, Stack, VStack, Heading, HStack, Button, Input } from '@chakra-ui/react';
+import React from 'react'
+import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram, AiOutlineSend} from "react-icons/ai";
+import Link from "next/link";
+
+
 
 const Footer = () => {
-  return (
-    <footer className="bg-neutral-200   text-center text-white dark:bg-neutral-600 bottom-0 left-0 w-full">
-      <div className=" pt-4">
-        <div className="mb-4 flex justify-around">
-          <a href="#!" className=" text-neutral-800 dark:text-neutral-200">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14.032 4H9.96c-2.967 0-4.96 1.933-4.96 4.933v4.134H2v3.934h3.008V20h4.012v-7h2.995l.736-3.934h-3.73V8.113c0-.904.353-1.774 1.181-1.774h2.91v-3.34c-.508-.07-1.36-.175-2.582-.175-2.53 0-4.22 1.462-4.22 4.08v2.307H3.996v-4C3.996 4.35 5.606 2 9.793 2c1.478 0 2.808.105 3.185.155z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>{" "}
-          <a href="#!" className=" text-neutral-800 dark:text-neutral-200">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14.032 4H9.96c-2.967 0-4.96 1.933-4.96 4.933v4.134H2v3.934h3.008V20h4.012v-7h2.995l.736-3.934h-3.73V8.113c0-.904.353-1.774 1.181-1.774h2.91v-3.34c-.508-.07-1.36-.175-2.582-.175-2.53 0-4.22 1.462-4.22 4.08v2.307H3.996v-4C3.996 4.35 5.606 2 9.793 2c1.478 0 2.808.105 3.185.155z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>{" "}
-          <a href="#!" className=" text-neutral-800 dark:text-neutral-200">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14.032 4H9.96c-2.967 0-4.96 1.933-4.96 4.933v4.134H2v3.934h3.008V20h4.012v-7h2.995l.736-3.934h-3.73V8.113c0-.904.353-1.774 1.181-1.774h2.91v-3.34c-.508-.07-1.36-.175-2.582-.175-2.53 0-4.22 1.462-4.22 4.08v2.307H3.996v-4C3.996 4.35 5.606 2 9.793 2c1.478 0 2.808.105 3.185.155z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>{" "}
-          <a href="#!" className=" text-neutral-800 dark:text-neutral-200">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14.032 4H9.96c-2.967 0-4.96 1.933-4.96 4.933v4.134H2v3.934h3.008V20h4.012v-7h2.995l.736-3.934h-3.73V8.113c0-.904.353-1.774 1.181-1.774h2.91v-3.34c-.508-.07-1.36-.175-2.582-.175-2.53 0-4.22 1.462-4.22 4.08v2.307H3.996v-4C3.996 4.35 5.606 2 9.793 2c1.478 0 2.808.105 3.185.155z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
-          {/* Add more social links here */}
-        </div>
-      </div>
+    return (
+        <Box bgColor={"blackAlpha.900"} minH={'40'} p={'16'} color={"white"}>
+            <Stack direction={['column', 'row']}>
+                <VStack alignItems={"stretch"} w={'full'} px={'4'}>
+                    <Heading size='md' textTransform={'uppercase'} textAlign={["center","left"]}>
+                        for latest updates
+                    </Heading>
+                    <HStack
+                        borderBottom={'2px solid white'}
+                        py={'2'}
+                    >
+                        <Input
+                            placeholder="Enter Email Here..."
+                            border={"none"}
+                            borderRadius={'none'}
+                            outline={'none'}
+                            focusBorderColor='none'
+                        />
 
-      <div className="bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
-        © 2023 Copyright:
-        <a
-          className="text-neutral-800 dark:text-neutral-400"
-          href="https://tailwind-elements.com/"
-        >
-          Oxign
-        </a>
-      </div>
-    </footer>
-  );
-};
+                        <Button
+                            p={'0'}
+                            color={'#5853ff'}
+                            variant={'ghost'}
+                            borderRadius={'0 20px 20px 0'}
+                        >
+                            <AiOutlineSend size={20} />
+                        </Button>
+                    </HStack>
+                </VStack >
+
+                <VStack 
+                w={'full'} 
+                borderLeft={['none','1px solid white']}
+                borderRight={['none','1px solid white']} 
+                >
+                    <Heading textTransform={'uppercase'} textAlign={'center'} color='#5853ff'>
+                        Oxign
+                    </Heading>
+                    {/* <Text>All rights received</Text> */}
+                    <div style={{textAlign:"center" }}>
+                    <a href="mailto:OXIGN@GMAIL.COM" >SEND MAIL TO- <span style={{color: "#5853ff"
+                    }}>OXIGN@GMAIL.COM</span></a>
+                    </div>
+                </VStack>
+
+                <VStack w={"full"} alignItems={"center"} justifyContent={"center"}>
+               <div className=' p-2 flex items-center rounded-tl-[2rem] rounded-tr-[2rem] rounded-br-[2rem] rounded-bl-[2rem]'>
+                
+                <article className='flex mx-1 my-0 flex-wrap justify-center'>
+                   
+
+                    <Link href="https://www.google.com/" target="_blank">
+                    <div className='m-2 h-14 w-14 bg-white rounded-[50%] flex flex-col items-center justify-center text-black cursor-pointer transition-all duration-500 hover:invert animate-waving-hand' style={{
+                        animationDelay: "0.3s",
+                        }}>
+                        <AiFillGoogleCircle className='text-2xl'/>
+                        <p className='text-[8px] font-black color-[#090c31]'>Google</p>
+                    </div>
+                    </Link>
+
+                    <Link href="https://www.amazon.com" target="_blank">
+                    <div className='m-2 h-14 w-14 bg-white rounded-[50%] flex flex-col items-center justify-center text-black cursor-pointer transition-all duration-500 hover:invert animate-waving-hand' style={{
+                        animationDelay: "0.5s",
+                        }}>
+                        <AiFillAmazonCircle className='text-2xl'/>
+                        <p className='text-[8px] font-black color-[#090c31]'>Amazon</p>
+                    </div>
+                    </Link>
+
+
+                    <Link href="https://www.youtube.com/" target="_blank">
+
+                    <div className='m-2 h-14 w-14 bg-white rounded-[50%] flex flex-col items-center justify-center text-black cursor-pointer transition-all duration-500 hover:invert animate-waving-hand' style={{
+                        animationDelay: "0.7s",
+                        }}>
+                        <AiFillYoutube className='text-2xl'/>
+                        <p className='text-[8px] font-black color-[#090c31]'>Youtube</p>
+                    </div>
+                    </Link>
+
+
+                    <Link href="https://www.instagram.com/" target="_blank">
+
+                    <div className='m-2 h-14 w-14 bg-white rounded-[50%] flex flex-col items-center justify-center text-black cursor-pointer transition-all duration-500 hover:invert animate-waving-hand' style={{
+                        animationDelay: "1s",
+                        }}>
+                        <AiFillInstagram className='text-2xl'/>
+                        <p className='text-[8px] font-black color-[#090c31]'>Instagram</p>
+                    </div>
+                    </Link>
+
+
+             
+                </article>
+            </div>
+               </VStack>
+
+            </Stack>
+
+        </Box>
+    )
+}
 
 export default Footer;
