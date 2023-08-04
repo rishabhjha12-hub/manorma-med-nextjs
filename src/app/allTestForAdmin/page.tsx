26e0396cd8deb89d19cedc2dce8f21956e9353c8
Prakash Chand <Prakash0719pc@gmail.com>
2023-08-04 18:47:16 +0530

    search

--- src/app/allTestForAdmin/page.tsx
+++ src/app/allTestForAdmin/page.tsx
@@ -2,18 +2,25 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { VscChromeClose, VscCheck } from "react-icons/vsc";
+import { VscChromeClose } from "react-icons/vsc";
 import { toast } from "react-hot-toast";
+import Loader from "../component/Loader";
+import BeatLoader from "react-spinners/BeatLoader";
 
 const AllTestForAdmin = () => {
   const [labTests, setLabTests] = useState([]);
-
+  const [loader, setloader] = useState(true);
+  const [betLoader, setBetLoader] = useState(false);
+  const [searchData, setSearchData] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     async function fetchLabTests() {
       try {
+        setloader(true);
         const response = await axios.get("/api/getAllLabtest");
+        setloader(false);
         setLabTests(response.data);
-        console.log(response.data);
+        setSearchData(response.data);
       } catch (error) {
         console.error("Error fetching lab tests:");
       }
@@ -22,7 +29,9 @@ const AllTestForAdmin = () => {
   }, []);
   const handleDelete = async (id: any) => {
     try {
+      setBetLoader(true);
       await axios.delete(`/api/deleteTest/${id}`);
+      setBetLoader(false);
       const updatedLabTests = labTests.filter((test: any) => test._id !== id);
       setLabTests(updatedLabTests);
       toast.success("Deleted Successfully");
@@ -32,12 +41,10 @@ const AllTestForAdmin = () => {
   };
   const handleMarkFeatured = async (id: any, isFeatured: any) => {
     try {
-        
       await axios.put("/api/makeFeatured", { id, isFeatured: !isFeatured });
-      if(isFeatured){
+      if (isFeatured) {
         toast.success(" Marked not featured Successfully");
-      }
-      else{
+      } else {
         toast.success(" Marked featured Successfully");
       }
       setLabTests((prevLabTests: any) =>
@@ -45,96 +52,152 @@ const AllTestForAdmin = () => {
           test._id === id ? { ...test, isFeatured: !isFeatured } : test
         )
       );
-      
     } catch (error) {
       console.error("Error updating lab test:");
     }
   };
-  return (
-    <>
-      <table className="min-w-full text-center text-sm font-light">
-        <thead className="border-b font-medium dark:border-neutral-500">
-          <tr>
-            <th scope="col" className="px-6 py-4">
-              Serial Number
-            </th>
-            <th scope="col" className="px-6 py-4">
-              Test name
-            </th>
-            <th scope="col" className="px-6 py-4">
-              Test price
-            </th>
-            <th scope="col" className="px-6 py-4">
-              govt (Test price)
-            </th>
-            <th scope="col" className="px-6 py-4">
-              Result
-            </th>
-            <th scope="col" className="px-6 py-4">
-              Delete Appointment
-            </th>
-            <th scope="col" className="px-6 py-4">
-              Status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {labTests.map((appointment: any, index) => {
-            return (
-              <>
-                <tr
-                  key={appointment._id}
-                  className="border-b dark:border-neutral-500 bg-blue-300"
-                >
-                  <td className="whitespace-nowrap px-6 py-4 font-medium">
-                    {index + 1}
-                  </td>
-                  <td className="whitespace-nowrap px-6 py-4">
-                    {appointment.testName}
-                  </td>
-                  <td className="whitespace-nowrap px-6 py-4">
-                    {appointment.price}
-                  </td>
-                  <td className="whitespace-nowrap px-6 py-4">
-                    {appointment.govPrice
-                      ? `${appointment.govPrice}`
-                      : "Not avilable"}
-                  </td>
-                  <td className="whitespace-nowrap px-6 py-4">
-                    {appointment.expectedResults}
-                  </td>
-                  <td
-                    className="whitespace-nowrap px-6 py-4 "
-                    onClick={() => handleDelete(appointment._id)}
+// ---------------------------------------------------------------------- search --------------
+  const handleInputChange = (e: any) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    if (search == "") {
+      toast.success("Please enter test name");
+    } else {
+      const updatedFilter = searchData.filter(
+        (check: any) => check.testName.toLowerCase().includes(search.toLowerCase())
+      );
+      if (updatedFilter.length == 0) {
+        toast.error(" Test Not found");
+        setSearch("");
+      } else {
+        setLabTests(updatedFilter);
+        setSearch("");
+      }
+    }
+  };
+
+  if (loader == true) {
+    return <Loader />;
+  } else if (labTests.length == 0) {
+    return <p>No data found</p>;
+  } else {
+    return (
+      <>
+        <div>
+          <input
+            type="text"
+            value={search}
+            onChange={handleInputChange}
+            className="border rounded-l-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-300" placeholder="Search..."
+          />
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300" onClick={handleSearchClick} >Search</button>
+        </div>
+
+        
+        <table className="min-w-full text-center text-sm font-light">
+          <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th scope="col" className="px-6 py-4">
+                Serial Number
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Test name
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Test price
+              </th>
+              <th scope="col" className="px-6 py-4">
+                govt (Test price)
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Result
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Delete Appointment
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Status
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {labTests.map((appointment: any, index) => {
+              return (
+                <>
+                  <tr
+                    key={appointment._id}
+                    className="border-b dark:border-neutral-500 bg-blue-300"
                   >
-                    <button className="bg-black text-white p-2 rounded hover:scale-110 duration-500 capitalize font-medium">
-                      <div className="flex justify-around items-center">
-                        Delete
-                        <VscChromeClose color="#FF7276" />
-                      </div>
-                    </button>
-                  </td>
-                  <td
-                    className="whitespace-nowrap px-6 py-4"
-                    onClick={() =>
-                      handleMarkFeatured(
-                        appointment._id,
-                        appointment.isFeatured
-                      )
-                    }
-                  >
-                    <button className="bg-black text-white p-2 rounded hover:scale-110 duration-500 capitalize font-medium">
-                      {appointment.isFeatured ? "Mark not featured" : "mark featured"}
-                    </button>
-                  </td>
-                </tr>
-              </>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
-  );
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">
+                      {index + 1}
+                    </td>
+                    <td className="whitespace-nowrap px-6 py-4">
+                      {appointment.testName}
+                    </td>
+                    <td className="whitespace-nowrap px-6 py-4">
+                      {appointment.price}
+                    </td>
+                    <td className="whitespace-nowrap px-6 py-4">
+                      {appointment.govPrice
+                        ? `${appointment.govPrice}`
+                        : "Not avilable"}
+                    </td>
+                    <td className="whitespace-nowrap px-6 py-4">
+                      {appointment.expectedResults}
+                    </td>
+                    <td
+                      className="whitespace-nowrap px-6 py-4 "
+                      onClick={() => handleDelete(appointment._id)}
+                    >
+                      <button
+                        className="bg-black text-white p-2 rounded hover:scale-110 duration-500 capitalize font-medium"
+                        disabled={betLoader ? true : false}
+                      >
+                        {betLoader ? (
+                          <div className="flex justify-evenly items-center">
+                            Deleting
+                            <BeatLoader
+                              className=""
+                              color={"#D0021B"}
+                              size={10}
+                              aria-label="Loading Spinner"
+                              data-testid="loader"
+                            />
+                          </div>
+                        ) : (
+                          <div className="flex justify-around items-center">
+                            Delete
+                            <VscChromeClose color="#FF7276" />
+                          </div>
+                        )}
+                      </button>
+                    </td>
+                    <td
+                      className="whitespace-nowrap px-6 py-4"
+                      onClick={() =>
+                        handleMarkFeatured(
+                          appointment._id,
+                          appointment.isFeatured
+                        )
+                      }
+                    >
+                      <button className="bg-black text-white p-2 rounded hover:scale-110 duration-500 capitalize font-medium">
+                        {appointment.isFeatured
+                          ? "Mark not featured"
+                          : "mark featured"}
+                      </button>
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 };
 
 export default AllTestForAdmin;
