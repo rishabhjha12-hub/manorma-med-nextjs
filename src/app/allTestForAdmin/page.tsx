fb4b451f8d1f18c172148938339faf26490e0669
Anshuman Negi <negsanshuman14@gmail.com>
2023-08-05 12:03:11 +0530

    remove dark class from all pages except allAppointment

--- src/app/allTestForAdmin/page.tsx
+++ src/app/allTestForAdmin/page.tsx
@@ -132,7 +132,7 @@ const AllTestForAdmin = () => {
           <p>No data found</p>
         ) : (
           <table className="min-w-full text-center text-sm font-light">
-            <thead className="border-b font-medium dark:border-neutral-500">
+            <thead className="border-b font-medium border-slate-300">
               <tr>
                 <th scope="col" className="px-6 py-4">
                   Serial Number
@@ -163,7 +163,7 @@ const AllTestForAdmin = () => {
                   <>
                     <tr
                       key={appointment._id}
-                      className="border-b dark:border-neutral-500 bg-blue-300"
+                      className="border-b border-slate-300 bg-blue-300"
                     >
                       <td className="whitespace-nowrap px-6 py-4 font-medium">
                         {index + 1}
