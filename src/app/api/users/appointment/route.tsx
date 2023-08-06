29d43909a265060120d537e7a798e218abe338b6
rishabhjha12-hub <76894646+rishabhjha12-hub@users.noreply.github.com>
2023-08-06 22:15:56 +0530

    added nodemailer

--- src/app/api/users/appointment/route.tsx
+++ src/app/api/users/appointment/route.tsx
@@ -1,10 +1,43 @@
-// pages/api/addAppointment.js
-
 import { connect } from "@/dbConfig/dbConfig";
 import LabTestAppointment from "../../../../models/appointment";
 import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 connect();
+
+// Function to send the email
+async function sendAppointmentConfirmationEmail(patientName: any,testName:any) {
+  const email = "rishabhjha0@gmail.com";
+  const transporter = nodemailer.createTransport({
+    // configure your email provider here
+    service: "gmail",
+    port: 465,
+    secure: true,
+    logger: true,
+    debug: true,
+    auth: {
+      user: "oxignpathlab@gmail.com",
+      pass: "ckvpyvzprcmjdfhw",
+    },
+    tls: {
+      rejectUnauthorized: true,
+    },
+  });
+
+  const mailOptions = {
+    from: "oxignpathlab@gmail.com",
+    to: email,
+    subject: "Appointment Confirmation",
+    text: `Dear ${patientName},\n\nYour appointment for ${testName} has been successfully booked. Thank you for choosing us.\n\nBest regards,\nThe Appointment Team`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Appointment confirmation email sent to:", email);
+  } catch (error) {
+    console.error("Error sending appointment confirmation email:", error);
+  }
+}
 
 export async function POST(request: NextRequest) {
   try {
@@ -34,6 +67,9 @@ export async function POST(request: NextRequest) {
     });
 
     await appointment.save();
+
+    // Send the appointment confirmation email
+    await sendAppointmentConfirmationEmail(patientName, testName);
 
     return NextResponse.json({
       message: "Appointment added successfully",
