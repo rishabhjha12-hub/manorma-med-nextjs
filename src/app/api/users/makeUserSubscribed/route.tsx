854dfb8b115f66fc9e401469819d0438e22083b7
rishabhjha12-hub <76894646+rishabhjha12-hub@users.noreply.github.com>
2023-08-06 14:08:15 +0530

    added subscribed

---
+++ src/app/api/users/makeUserSubscribed/route.tsx
@@ -0,0 +1,42 @@
+// pages/api/updateLabTest.js
+
+import { connect } from "@/dbConfig/dbConfig";
+import User from "../../../../models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+
+export async function PUT(request: NextRequest) {
+  if (request.method !== "PUT") {
+    return NextResponse.json(
+      { message: "Method Not Allowed" },
+      { status: 405 }
+    );
+  }
+
+  const { id } = await request.json();
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    user.isFeatured = true;
+    await user.save();
+
+    return NextResponse.json(
+      { message: "User updated successfully" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
