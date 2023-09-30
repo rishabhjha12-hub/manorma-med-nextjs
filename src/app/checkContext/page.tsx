5a1c9631566d0f7372f847a7aa1db2e63c4049b6
Rishabh Jha <rishabhjha0@gmail.com>
2023-09-30 20:00:43 +0530

    added context

--- src/app/checkContext/page.tsx
+++ src/app/checkContext/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import React from 'react'
-import { useContext } from 'react';
+import { useContext,useEffect } from 'react';
 import { UserContext } from '../context/UserContext';
 
 const CheckContext = () => {
     const { checkUser } = useContext(UserContext);
-    // const {checkUser, setCheckUser} = useContext(UserContext);
     console.log(checkUser,"this is user abcd");
+   
   return (
-    <div>CHeckUser</div>
+    <div>CHeckUser-{checkUser?.username}</div>
   )
 }
 
