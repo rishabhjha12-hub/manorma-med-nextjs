bc3713b9f15f3b1d788bd32505d2dc52416e15cf
letzkhelo <letzkhello@gmail.com>
2023-10-01 13:08:38 +0530

    update

--- src/app/component/Loader.tsx
+++ src/app/component/Loader.tsx
@@ -1,14 +1,32 @@
-import { VStack,Box,Spinner } from '@chakra-ui/react';
-import React from 'react'
 
-const Loader = () => {
+import React from 'react';
+import { css } from '@emotion/react';
+import { PacmanLoader } from 'react-spinners';
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
+
+const Loader = (props: any) => {
+  const { className, ...restProps } = props;
+
   return (
-    <VStack h={"90vh"} justifyContent={"center"}>
-      <Box  transform={"scale(3)"}>
-         <Spinner size={"xl"} />
-      </Box>
-    </VStack>
-  )
-}
+    <div className={`loader-container ${className}`} css={override} {...restProps}>
+      <PacmanLoader color="#36D7B7" loading={true} size={30} />
+      <style jsx>{`
+        .loader-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default Loader;
+
+
